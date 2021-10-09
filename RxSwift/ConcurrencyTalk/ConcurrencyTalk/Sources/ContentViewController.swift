7db9e7824bd81b5b8f997d7b1a8003c02632de9e
Alex Manarpies <alex@jarroo.com>
2021-10-09 22:05:49 +0200

    Init

---
+++ RxSwift/ConcurrencyTalk/ConcurrencyTalk/Sources/ContentViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ContentViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
